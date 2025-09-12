ded6de4da4711c74793184762465702e136bc22d
Léo Milbor <20482270+leo-milbor@users.noreply.github.com>
2025-09-13 01:40:15 +0800

    fix: mini pair

--- lua/plugins/quality-of-life.lua
+++ lua/plugins/quality-of-life.lua
@@ -1,6 +1,12 @@
 return {
   -- Automatically add pair i.e. (), [], {}, etc.
-  { 'nvim-mini/mini.pairs',       version = '*' },
+  {
+    'nvim-mini/mini.pairs',
+    version = '*',
+    config = function()
+      require('mini.pairs').setup()
+    end
+  },
   -- Show indent level.
   { 'nvim-mini/mini.indentscope', version = '*' },
 }
