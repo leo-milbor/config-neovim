38d9f5b3b28512d01a7366808eef917a8705a4b8
Léo Milbor <20482270+leo-milbor@users.noreply.github.com>
2025-09-21 01:00:50 +0800

    refacto: use ChatGPT.nvim for ai

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,3 +1,28 @@
 return {
-  { 'aduros/ai.vim' }
+	{
+		"jackMort/ChatGPT.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+			"folke/trouble.nvim", -- optional
+			"nvim-telescope/telescope.nvim",
+		},
+		config = function()
+			require("chatgpt").setup({
+				api_key_cmd = "echo $OPENAI_API_KEY", -- load from env var
+				openai_params = {
+					model = "gpt-4o-mini", -- or gpt-3.5-turbo
+					temperature = 0.3,
+					max_tokens = 1200,
+				},
+				popup_layout = { width = "80%", height = "80%" },
+				popup_window = { border = { style = "rounded" } },
+			})
+
+			-- Keymaps
+			vim.keymap.set("n", "<leader>ac", ":ChatGPT<CR>", { desc = "Open ChatGPT agent" })
+			vim.keymap.set("v", "<leader>ae", ":ChatGPTEditWithInstructions<CR>", { desc = "Edit code with GPT" })
+		end,
+	},
 }
--- lua/plugins/cheatsheet.lua
+++ lua/plugins/cheatsheet.lua
@@ -1,31 +1,32 @@
 return {
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-    end,
-    opts = {},
-    config = function()
-      local wk = require("which-key")
-      wk.add({
-        { "<leader>",   group = "Extended functionality" },
-        { "<leader>w",  group = "Windows",               proxy = "<c-w>" }, -- proxy to window mappings
-        { "<leader>b",  group = "Buffer" },
-        { "<leader>s",  group = "Search & replace" },
-        { "<leader>i",  group = "Show information" },
-        { "<leader>d",  group = "Debugging" },
-        { "<leader>f",  group = "Find" },
-        { "<leader>g",  group = "Go to or LSP action" },
-        { "<leader>n",  group = "Note taking" },
-        { "<leader>r",  group = "Refactoring" },
-        { "<leader>rE", group = "Extract (file)" },
-        { "<leader>re", group = "Extract" },
-        { "<leader>ri", group = "Inline" },
-        { "<leader>t",  group = "Testing" },
-        { "<leader>e",  group = "Execute code" },
-      })
-    end,
-  },
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {},
+		config = function()
+			local wk = require("which-key")
+			wk.add({
+				{ "<leader>", group = "Extended functionality" },
+				{ "<leader>a", group = "AI tools" },
+				{ "<leader>b", group = "Buffer" },
+				{ "<leader>d", group = "Debugging" },
+				{ "<leader>e", group = "Execute code" },
+				{ "<leader>f", group = "Find" },
+				{ "<leader>g", group = "Go to or LSP action" },
+				{ "<leader>i", group = "Show information" },
+				{ "<leader>n", group = "Note taking" },
+				{ "<leader>r", group = "Refactoring" },
+				{ "<leader>re", group = "Extract" },
+				{ "<leader>rE", group = "Extract (file)" },
+				{ "<leader>ri", group = "Inline" },
+				{ "<leader>s", group = "Search & replace" },
+				{ "<leader>t", group = "Testing" },
+				{ "<leader>w", group = "Windows", proxy = "<c-w>" }, -- proxy to window mappings
+			})
+		end,
+	},
 }
