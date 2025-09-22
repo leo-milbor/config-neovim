ff9969f2dbecee96882c0fb487b2bd3173691504
Léo Milbor <20482270+leo-milbor@users.noreply.github.com>
2025-09-23 00:46:29 +0800

    refato: moving stuff around

--- lua/plugins/quality-of-life.lua
+++ lua/plugins/quality-of-life.lua
@@ -1,12 +1,29 @@
 return {
-  -- Automatically add pair i.e. (), [], {}, etc.
-  {
-    'nvim-mini/mini.pairs',
-    version = '*',
-    config = function()
-      require('mini.pairs').setup()
-    end
-  },
-  -- Show indent level.
-  { 'nvim-mini/mini.indentscope', version = '*' },
+	-- Surround text objects easily
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+	-- Automatically add pair i.e. (), [], {}, etc.
+	{
+		"nvim-mini/mini.pairs",
+		version = "*",
+		config = function()
+			require("mini.pairs").setup()
+		end,
+	},
+	-- Show indent level.
+	{
+		"nvim-mini/mini.indentscope",
+		version = "*",
+		config = function()
+			require("mini.indentscope").setup()
+		end,
+	},
 }
