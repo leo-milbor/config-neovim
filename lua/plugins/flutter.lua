d6ccaebe16950df2b70361dfda21069e8bdf7a5c
Léo Milbor <20482270+leo-milbor@users.noreply.github.com>
2025-09-23 23:55:09 +0800

    refacto: config for flutter

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -1,10 +1,53 @@
+local mise_path = vim.fn.expand("~") .. ".local/share/mise/installs/flutter/3.35.4-stable/bin/"
+local flutter_sdk_path = mise_path .. "flutter"
+local dart_sdk_path = mise_path .. "dart"
+
 return {
-  {
-    'akinsho/flutter-tools.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'stevearc/dressing.nvim',
-    },
-    config = true,
-  }
+	{
+		"akinsho/flutter-tools.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"stevearc/dressing.nvim",
+			"mfussenegger/nvim-dap",
+		},
+		config = function()
+			-- require("flutter-tools").setup({
+			-- 	flutter_path = flutter_sdk_path,
+			--
+			-- })
+			local dap = require("dap")
+			dap.adapters.flutter_test_debug = {
+				type = "executable",
+				command = vim.fn.stdpath("data") .. "/mason/bin/dart-debug-adapter",
+				args = { "flutter_test" },
+			}
+			local flutter_test_run_config = {
+				type = "flutter_test_debug",
+				request = "launch",
+				name = "Debug Flutter Test",
+				dartSdkPath = dart_sdk_path,
+				flutterSdkPath = flutter_sdk_path,
+				program = "${file}",
+				args = {}, -- args are passed down to the underlying `flutter test`
+				cwd = "${workspaceFolder}",
+			}
+			dap.adapters.dart = {
+				type = "executable",
+				command = vim.fn.stdpath("data") .. "/mason/bin/dart-debug-adapter",
+				args = { "flutter" },
+			}
+			dap.configurations.dart = {
+				{
+					type = "dart",
+					request = "launch",
+					name = "Launch Flutter",
+					dartSdkPath = dart_sdk_path,
+					flutterSdkPath = flutter_sdk_path,
+					program = "${workspaceFolder}/lib/main.dart",
+					cwd = "${workspaceFolder}",
+				},
+			}
+		end,
+	},
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,47 +1,46 @@
 local servers = {
-  "lua_ls",        -- lua
-  "bashls",        -- bash
-  "gopls",         -- go
-  "clojure_lsp",   -- clojure
-  "rust_analyzer", -- rust
-  "html",          -- html
-  "jsonls",        -- json
-  "dockerls",      -- dockerfile
-  "pyright",       -- python
-  "marksman",      -- markdown
-  "taplo",         -- toml
-  "yamlls",        -- yaml
-  "dockerls",      -- dockerfile
-  -- "fsautocomplete", -- F#
-  -- "rnix-lsp",       -- Nix
+	"lua_ls", -- lua
+	"bashls", -- bash
+	"gopls", -- go
+	"clojure_lsp", -- clojure
+	"rust_analyzer", -- rust
+	"html", -- html
+	"jsonls", -- json
+	"dockerls", -- dockerfile
+	"pyright", -- python
+	"marksman", -- markdown
+	"taplo", -- toml
+	"yamlls", -- yaml
+	"dockerls", -- dockerfile
+	-- "fsautocomplete", -- F#
+	-- "rnix-lsp",       -- Nix
 }
 
 return {
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      {
-        "neovim/nvim-lspconfig",
-        config = function()
-          vim.lsp.config("*", {
-            capabilities = vim.lsp.protocol.make_client_capabilities()
-          })
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			{
+				"neovim/nvim-lspconfig",
+				config = function()
+					vim.lsp.config("*", {
+						capabilities = vim.lsp.protocol.make_client_capabilities(),
+					})
 
+					vim.keymap.set("n", "grf", vim.lsp.buf.format, { desc = "Format" })
 
-          vim.keymap.set("n", "grf", vim.lsp.buf.format, { desc = "Format" })
-
-          -- format on save
-          vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
-        end,
-      },
-    },
-    config = function()
-      require("mason-lspconfig").setup({
-        automatic_enable = true,
-        ensure_installed = servers,
-      })
-    end,
-  },
+					-- format on save
+					vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
+				end,
+			},
+		},
+		config = function()
+			require("mason-lspconfig").setup({
+				automatic_enable = true,
+				ensure_installed = servers,
+			})
+		end,
+	},
 }
