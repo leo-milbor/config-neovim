913e9756c1dff95e48214baa71672159e79686a3
Léo Milbor <20482270+leo-milbor@users.noreply.github.com>
2025-10-13 23:18:08 +0800

    refacto: conform config

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
 --Make sure lazy.nvim is installed.
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('config.keymaps') -- non-plugins settings
-require('config.options') -- non-plugins settings
-require('lazy').setup('plugins')
+require("config.keymaps") -- non-plugins settings
+require("config.options") -- non-plugins settings
+require("lazy").setup("plugins")
--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,25 +1,27 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      python = { "black" },
-      javascript = { "prettierd" },
-      typescript = { "prettierd" },
-      json = { "prettierd" },
-      html = { "prettierd" },
-      css = { "prettierd" },
-      scss = { "prettierd" },
-      markdown = { "prettierd" },
-      yaml = { "prettierd" },
-      toml = { "taplo" },
-      sh = { "shfmt" },
-      csharp = { "csharpier" },
-    },
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
-  },
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "black" },
+			javascript = { "prettierd" },
+			typescript = { "prettierd" },
+			json = { "prettierd" },
+			html = { "prettierd" },
+			css = { "prettierd" },
+			scss = { "prettierd" },
+			markdown = { "prettierd" },
+			yaml = { "prettierd" },
+			toml = { "taplo" },
+			sh = { "shfmt" },
+			csharp = { "csharpier" },
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	},
+	config = function(_, opts)
+		require("conform").setup(opts)
+	end,
 }
