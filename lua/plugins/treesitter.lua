fa838143e46ca42f57f2844c3a23178c7548a450
Léo Milbor <20482270+leo-milbor@users.noreply.github.com>
2025-10-15 22:29:16 +0800

    feat: make treesitter consider xaml & axaml file as xml

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -16,6 +16,13 @@ return {
 				},
 			})
 
+			vim.filetype.add({
+				extension = {
+					axaml = "xml",
+					xaml = "xml",
+				},
+			})
+
 			-- Ensure treesitter is loaded when entering buffer.
 			vim.keymap.set("n", "<leader>th", vim.treesitter.start, { desc = "start treesitter" })
 			vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
