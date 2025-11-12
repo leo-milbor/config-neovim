7f077082360fa9863ef0d06ed6ec2ab2fdfc82c0
Léo Milbor <20482270+leo-milbor@users.noreply.github.com>
2025-11-13 00:01:18 +0800

    feat: add dotnet support + avalonia

---
+++ lua/plugins/dotnet.lua
@@ -0,0 +1,24 @@
+-- start avalonia LSP in axaml files
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	pattern = { "*.axaml" },
+	callback = function(event)
+		vim.lsp.start({
+			name = "avalonia",
+			cmd = { "avalonia-ls" },
+			root_dir = vim.fn.getcwd(),
+		})
+	end,
+})
+
+vim.filetype.add({
+	extension = {
+		axaml = "xml",
+		xaml = "xml",
+	},
+})
+
+return {
+	-- {
+	-- 	"OmniSharp/omnisharp-vim",
+	-- },
+}
