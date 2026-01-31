366929fcb87acba588cf9fc4e0b9253019d3c154
Léo Milbor <20482270+leo-milbor@users.noreply.github.com>
2026-02-01 00:18:46 +0800

    feat: add some file extension to treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,38 +1,41 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		lazy = false,
-		branch = "main",
-		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter").install("stable")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    branch = "main",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").install("stable")
 
-			-- Ensure treesitter is loaded when entering buffer.
-			vim.keymap.set("n", "<leader>th", vim.treesitter.start, { desc = "start treesitter" })
-			vim.api.nvim_create_autocmd({ "FileType" }, {
-				pattern = {
-					"go",
-					"mod",
-					"sum",
-					"rs",
-					"lua",
-					"cs",
-					"sh",
-					"ex",
-					"exs",
-					"dart",
-					"xml",
-					"html",
-					"json",
-					"toml",
-					"yaml",
-				},
-				callback = function()
-					vim.treesitter.start()
-					vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
-					vim.bo[0][0].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-				end,
-			})
-		end,
-	},
+      -- Ensure treesitter is loaded when entering buffer.
+      vim.keymap.set("n", "<leader>th", vim.treesitter.start, { desc = "start treesitter" })
+      vim.api.nvim_create_autocmd({ "FileType" }, {
+        pattern = {
+          "go",
+          "mod",
+          "sum",
+          "rs",
+          "lua",
+          "cs",
+          "sh",
+          "ex",
+          "exs",
+          "dart",
+          "make",
+          "md",
+          "awk",
+          "xml",
+          "html",
+          "json",
+          "toml",
+          "yaml",
+        },
+        callback = function()
+          vim.treesitter.start()
+          vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+          vim.bo[0][0].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
+  },
 }
