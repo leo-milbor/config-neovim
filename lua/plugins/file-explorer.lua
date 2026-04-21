1dca608f50694603c1c02fb7e4ba6d0ad6ab7540
Léo Milbor <20482270+leo-milbor@users.noreply.github.com>
2026-04-22 00:30:12 +0800

    chore: update deps

--- lua/plugins/file-explorer.lua
+++ lua/plugins/file-explorer.lua
@@ -1,32 +1,32 @@
 return {
-  {
-    'stevearc/oil.nvim',
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {
-      skip_confirm_for_simple_edits = true,
-    },
-    -- Optional dependencies
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-    lazy = false,
+	{
+		"stevearc/oil.nvim",
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {
+			skip_confirm_for_simple_edits = true,
+		},
+		-- Optional dependencies
+		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+		-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+		lazy = false,
 
-    config = function()
-      require("oil").setup()
-      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-    end,
-  },
-  {
-    -- diagnostics signs
-    "JezerM/oil-lsp-diagnostics.nvim",
-    dependencies = { "stevearc/oil.nvim" },
-    opts = {}
-  },
-  {
-    -- git signs
-    "benomahony/oil-git.nvim",
-    dependencies = { "stevearc/oil.nvim" },
-    -- No opts or config needed! Works automatically
-  },
+		config = function()
+			require("oil").setup()
+			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		end,
+	},
+	{
+		-- diagnostics signs
+		"JezerM/oil-lsp-diagnostics.nvim",
+		dependencies = { "stevearc/oil.nvim" },
+		opts = {},
+	},
+	{
+		-- git signs
+		"benomahony/oil-git.nvim",
+		dependencies = { "stevearc/oil.nvim" },
+		-- No opts or config needed! Works automatically
+	},
 }
--- lua/plugins/refactor.lua
+++ lua/plugins/refactor.lua
@@ -2,8 +2,7 @@ return {
 	{
 		"ThePrimeagen/refactoring.nvim",
 		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
+			"lewis6991/async.nvim",
 		},
 		lazy = false,
 		config = function()
