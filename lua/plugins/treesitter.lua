1fd7a889f0913e53e6bd2af548713dd6b2605d7b
Léo Milbor <20482270+leo-milbor@users.noreply.github.com>
2026-04-26 00:50:41 +0800

    feat: re-add treesitter text object

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -54,4 +54,59 @@ return {
 			})
 		end,
 	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		init = function()
+			-- Disable entire built-in ftplugin mappings to avoid conflicts.
+			-- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+			vim.g.no_plugin_maps = true
+		end,
+		config = function()
+			require("nvim-treesitter-textobjects").setup({
+				select = {
+					-- Automatically jump forward to textobj, similar to targets.vim
+					lookahead = true,
+					-- You can choose the select mode (default is charwise 'v')
+					--
+					-- Can also be a function which gets passed a table with the keys
+					-- * query_string: eg '@function.inner'
+					-- * method: eg 'v' or 'o'
+					-- and should return the mode ('v', 'V', or '<c-v>') or a table
+					-- mapping query_strings to modes.
+					selection_modes = {
+						["@parameter.outer"] = "v", -- charwise
+						["@function.outer"] = "V", -- linewise
+						-- ['@class.outer'] = '<c-v>', -- blockwise
+					},
+					-- If you set this to `true` (default is `false`) then any textobject is
+					-- extended to include preceding or succeeding whitespace. Succeeding
+					-- whitespace has priority in order to act similarly to eg the built-in
+					-- `ap`.
+					--
+					-- Can also be a function which gets passed a table with the keys
+					-- * query_string: eg '@function.inner'
+					-- * selection_mode: eg 'v'
+					-- and should return true of false
+					include_surrounding_whitespace = false,
+				},
+			})
+
+			-- keymaps
+			-- You can use the capture groups defined in `textobjects.scm`
+			local select_textobject = require("nvim-treesitter-textobjects.select").select_textobject
+			vim.keymap.set({ "x", "o" }, "af", function()
+				select_textobject("@function.outer", "textobjects")
+			end, { desc = "function" })
+			vim.keymap.set({ "x", "o" }, "if", function()
+				select_textobject("@function.inner", "textobjects")
+			end, { desc = "function" })
+			vim.keymap.set({ "x", "o" }, "ac", function()
+				select_textobject("@class.outer", "textobjects")
+			end, { desc = "class" })
+			vim.keymap.set({ "x", "o" }, "ic", function()
+				select_textobject("@class.inner", "textobjects")
+			end, { desc = "class" })
+		end,
+	},
 }
