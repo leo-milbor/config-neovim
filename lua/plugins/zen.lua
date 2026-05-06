f12c344d87be94c20c74a709dcab3386c45e7c58
Léo Milbor <20482270+leo-milbor@users.noreply.github.com>
2026-05-06 22:06:14 +0800

    feat: make zen mode wider

--- lua/plugins/zen.lua
+++ lua/plugins/zen.lua
@@ -1,18 +1,22 @@
 return {
-  {
-    "folke/zen-mode.nvim",
-    opts = {
-      alacritty = {
-        enabled = true,
-      },
-    },
-    config = function()
-      local zen = require("zen-mode")
-      local set_key = vim.keymap.set
+	{
+		"folke/zen-mode.nvim",
+		opts = {
+			alacritty = {
+				enabled = true,
+			},
+		},
+		config = function()
+			local zen = require("zen-mode")
+			local set_key = vim.keymap.set
 
-      set_key("n", "<leader>z", function()
-        zen.toggle({})
-      end, { desc = "Toggle zen mode." })
-    end,
-  },
+			set_key("n", "<leader>z", function()
+				zen.toggle({
+					window = {
+						width = 0.70,
+					},
+				})
+			end, { desc = "Toggle zen mode." })
+		end,
+	},
 }
