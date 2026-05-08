b06a92ff2ebe337657d625868621f6f15e0451e1
Léo Milbor <20482270+leo-milbor@users.noreply.github.com>
2026-05-08 13:43:09 +0800

    chore: clean some comments & default conf

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -9,18 +9,6 @@ return {
 		---@module 'blink.cmp'
 		---@type blink.cmp.Config
 		opts = {
-			-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-			-- 'super-tab' for mappings similar to vscode (tab to accept)
-			-- 'enter' for enter to accept
-			-- 'none' for no mappings
-			--
-			-- All presets have the following mappings:
-			-- C-space: Open menu or open docs if already open
-			-- C-n/C-p or Up/Down: Select next/previous item
-			-- C-e: Hide menu
-			-- C-k: Toggle signature help (if signature.enabled = true)
-			--
-			-- See :h blink-cmp-config-keymap for defining your own keymap
 			keymap = { preset = "enter" },
 
 			signature = { enabled = true },
@@ -29,7 +17,10 @@ return {
 				nerd_font_variant = "mono",
 			},
 
-			completion = { documentation = { auto_show = true } },
+			completion = {
+				documentation = { auto_show = true },
+				list = { selection = { preselect = false } },
+			},
 
 			sources = {
 				default = { "lazydev", "lsp", "path", "snippets", "buffer" },
