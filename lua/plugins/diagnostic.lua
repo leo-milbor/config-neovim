bc30cbce8bb54d898e93c3bdaee005525295978c
Léo Milbor <20482270+leo-milbor@users.noreply.github.com>
2026-05-19 17:11:12 +0800

    feat: add nvim-lint with <leader>li

--- lua/plugins/diagnostic.lua
+++ lua/plugins/diagnostic.lua
@@ -2,4 +2,31 @@ vim.keymap.set("n", "<leader>ie", function()
 	vim.diagnostic.open_float()
 end, { noremap = true, silent = true, desc = "Show line diagnostics" })
 
-return {}
+return {
+	{
+		"mfussenegger/nvim-lint",
+		lazy = true,
+		event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+		config = function()
+			local lint = require("lint")
+
+			lint.linters_by_ft = {
+				javascript = { "oxlint" },
+				typescript = { "oxlint" },
+				vue = { "oxlint" },
+				go = { "golangcilint" },
+			}
+			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+			-- vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			-- group = lint_augroup,
+			-- callback = function()
+			-- lint.try_lint()
+			-- end,
+			-- })
+
+			vim.keymap.set("n", "<leader>li", function()
+				lint.try_lint()
+			end, { desc = "Trigger linting for current file" })
+		end,
+	},
+}
