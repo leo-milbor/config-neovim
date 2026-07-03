132880378db9d297564a56b2339aa2b6ad1d3e10
Léo Milbor <20482270+leo-milbor@users.noreply.github.com>
2026-07-03 21:43:21 +0800

    feat(fuzzy): exclude openspec folder & find directory & zoxide find

--- lua/plugins/fuzzy-find.lua
+++ lua/plugins/fuzzy-find.lua
@@ -15,7 +15,41 @@ return {
 			-- find
 			{ "<leader>fa", "<CMD>FzfLua global<CR>", desc = "All" },
 			{ "<leader>fb", "<CMD>FzfLua buffers sort_mru=true sort_lastused=true<CR>", desc = "Buffers" },
-			{ "<leader>ff", "<CMD>FzfLua files<CR>", desc = "Files (Root Dir)" },
+			{
+				"<leader>ff",
+				function()
+					require("fzf-lua").files({ fd_opts = "--hidden --exclude .git --exclude openspec" })
+				end,
+				desc = "Files (Root Dir)",
+			},
+			{
+				"<leader>cd",
+				function()
+					require("fzf-lua").files({
+						fd_opts = "--type d --hidden --exclude .git --exclude openspec",
+						actions = {
+							["default"] = function(selected, opts)
+								if not selected or #selected == 0 then
+									return
+								end
+
+								-- Extract the clean path string from fzf selection data
+								local entry = require("fzf-lua").path.entry_to_file(selected[1], opts)
+
+								-- Convert relative path to absolute path based on your current working directory
+								local path = require("fzf-lua.path")
+								local abs_path = path.is_absolute(entry.path) and entry.path
+									or path.join({ opts.cwd or vim.uv.cwd(), entry.path })
+
+								-- Open the target folder using Oil
+								require("oil").open(abs_path)
+							end,
+						},
+					})
+				end,
+				desc = "Directories",
+			},
+			{ "<leader>fz", "<cmd>FzfLua zoxide<cr>", { desc = "Zoxide change directory" } },
 			{ "<leader>fg", "<CMD>FzfLua git_files<CR>", desc = "Files (git-files)" },
 			{ "<leader>fr", "<CMD>FzfLua oldfiles<CR>", desc = "Recent" },
 			{ "<leader>fc", "<CMD>FzfLua command_history<CR>", desc = "Command History" },
